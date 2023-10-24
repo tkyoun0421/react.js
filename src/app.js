057ee4c2136679fd7e24f8d673fc4c72cf6cc981
태관 <tkyoun0421@naver.com>
2023-10-24 14:59:22 +0900

    create Element

--- src/app.js
+++ src/app.js
@@ -1,42 +1,15 @@
-import { createDOM, render } from "./react";
+import { createDOM, createElement, render } from "./react";
 
-const vdom = {
-    tag: "p",
-    props: {},
-    children: [
-        {
-            tag: "h1",
-            props: {},
-            children: ["React 만들기"],
-        },
-        {
-            tag: "ul",
-            props: {},
-            children: [
-                {
-                    tag: "li",
-                    props: {
-                        style: "color:red",
-                    },
-                    children: ["첫번째 아이템"],
-                },
-                {
-                    tag: "li",
-                    props: {
-                        style: "color:blue",
-                    },
-                    children: ["두번째 아이템"],
-                },
-                {
-                    tag: "li",
-                    props: {
-                        style: "color:green",
-                    },
-                    children: ["세번째 아이템"],
-                },
-            ],
-        },
-    ],
-};
-
+const vdom = createElement(
+    "p",
+    {},
+    createElement("h1", {}, "React 만들기"),
+    createElement(
+        "ul",
+        {},
+        createElement("li", { style: "color:red" }, "첫번째 아이템 만들기"),
+        createElement("li", { style: "color:blue" }, "두번째 아이템 만들기"),
+        createElement("li", { style: "color:green" }, "세번째 아이템 만들기")
+    )
+);
 render(vdom, document.querySelector("#root"));
